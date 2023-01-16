def790a7890f59012947fc7bbb88623f96eae934
vyky1983 <veronica.cernenchii@mail.ru>
2023-01-16 17:31:07 +0300

    add file dog

--- src/dog.java
+++ src/dog.java
@@ -1,2 +1,9 @@
 public class dog {
+    String name;
+    String deep;
+    Double wek;
+
+    String getDog(){
+        return " Имя собаки--> "+ name+" Порода собаки --> " +" Вес собаки --> "+wek;
+    }
 }
--- src/main.java
+++ src/main.java
@@ -1,2 +1,10 @@
 public class main {
+    public static void main(String[] args) {
+        dog dik = new dog();
+        dik.name="Dick";
+        dik.deep="Tacsa";
+        dik.wek=25.6;
+        System.out.println(dik.getDog());
+    }
+
 }
