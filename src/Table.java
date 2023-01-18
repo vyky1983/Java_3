a5c6a3872af3b6bd8f739a06b6bcad5429ef21b6
vyky1983 <veronica.cernenchii@mail.ru>
2023-01-18 12:54:21 +0300

    add new inf

---
+++ src/Table.java
@@ -0,0 +1,9 @@
+public class Table {
+    double height;
+    double length;
+    double width;
+
+    double getTable(){
+        return height*length*width;
+    }
+}
--- src/main.java
+++ src/main.java
@@ -1,10 +1,16 @@
 public class main {
     public static void main(String[] args) {
-        dog dik = new dog();
-        dik.name="Dick";
-        dik.deep="Tacsa";
-        dik.wek=25.6;
-        System.out.println(dik.getDog());
+//        dog dik = new dog();
+//        dik.name="Dick";
+//        dik.deep="Tacsa";
+//        dik.wek=25.6;
+//        System.out.println(dik.getDog());
+
+        Table tab=new Table();
+        tab.width=35.25;
+        tab.length=15.2;
+        tab.height=69.6;
+        System.out.println(tab.getTable());
     }
 
 }
